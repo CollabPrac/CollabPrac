0d1bbe67aa23dd5e34d3867718f5570ccd3953d3
Jacob22Bishop <44359503+Jacob22Bishop@users.noreply.github.com>
2020-02-21 11:42:28 +0000

    Add files via upload
    Timer script

---
+++ Timerr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timerr : MonoBehaviour
+{
+	Image timerBar;
+	public float maxTime = 5f;
+	float timeRight;
+	public GameObject timesUpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		timesUpText.SetActive (false);
+		timerBar = GetComponent<Image> ();
+		timeRight = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (timeRight > 0) {
+			timeRight -= Time.deltaTime;
+			timerBar.fillAmount = timeRight / maxTime;
+		} else {
+			timesUpText.SetActive (true);
+			Time.timeScale = 0;
+		}
+    }
+}
