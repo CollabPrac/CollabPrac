b168c716b2f7c7179aac3fbcf3ad11b4b1d63b39
Jacob22Bishop <44359503+Jacob22Bishop@users.noreply.github.com>
2020-04-09 15:13:10 +0100

    Scene Game Over
    With Grid but no timer bar

---
+++ Assets/Jacob/SceneGameOver(incGrid)/Scripts/LoadAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAfterTime : MonoBehaviour
+{
+  [SerializeField]
+   private float delayBeforeLoading = 10f;
+   [SerializeField]
+   private string sceneNameToLoad;
+   private float timeElaspsed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+      timeElaspsed += Time.deltaTime;
+      if (timeElaspsed > delayBeforeLoading)
+      {
+        SceneManager.LoadScene (sceneNameToLoad);
+      }
+    }
+}
