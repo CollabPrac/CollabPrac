02285c310b882b4a6bc78040f8fcf30e1686e5ea
Jacob22Bishop <44359503+Jacob22Bishop@users.noreply.github.com>
2020-05-01 12:42:49 +0100

    Sound attempt two

---
+++ Assets/Jacob/sounds2/FXCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXCode : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip soundfile;
+
+    private AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public void PlayDownSound()
+    {
+        audioSrc.PlayOneShot(soundfile);
+    }
+}
