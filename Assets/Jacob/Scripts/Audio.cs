42aafe6cbab286c5ea05e12b671009054c2151e9
Jacob22Bishop <44359503+Jacob22Bishop@users.noreply.github.com>
2020-05-01 16:15:18 +0100

    Audio attempt (not pasted)
    Hopefully 0 errors

---
+++ Assets/Jacob/Scripts/Audio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+  [SerializeField]
+  private AudioClip soundfile;
+
+  private AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    void OnMouseDown ()
+    {
+      audioSrc.PlayOneShot(soundfile);
+    }
+}
