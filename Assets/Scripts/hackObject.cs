39c43b8292ac72a83868e3daa377910e76ba8426
MashySP <56076327+MashySP@users.noreply.github.com>
2020-05-12 14:52:28 +0100

    Improvements
    Example

--- Assets/Scripts/hackObject.cs
+++ Assets/Scripts/hackObject.cs
@@ -16,6 +16,15 @@ public class hackObject : MonoBehaviour
     public Transform End;
     float endPoint;
     int place = 0;
+    int LastObject = 0;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float lastPositionX;
+    public float lastPositionY;
+    public int moveUp;
+    public int moveDown;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +35,11 @@ public class hackObject : MonoBehaviour
     void Update()
     {
         Vector3 translation = new Vector3(0, 0, 0);
-        
+        if (transform.position.x >= maxX || transform.position.x <= minX || transform.position.y >= maxY || transform.position.y <= minY)
+        {
+            translation = new Vector3(lastPositionX, lastPositionY);
+        }
+
         if(transform.position.x >= endPoint)
         {
             SceneManager.LoadScene(1);
@@ -34,9 +47,12 @@ public class hackObject : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            place = place + 1;
-            translation += new Vector3(0, -2);
-            ActivePart.transform.Translate(translation);
+            if (place < 5)
+            {
+                place = place + 1;
+                translation += new Vector3(0, -2);
+                ActivePart.transform.Translate(translation);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -51,11 +67,17 @@ public class hackObject : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
+            lastPositionX = transform.position.x;
+            lastPositionY = transform.position.y;
             if (place == 0)
             {
-                Rigidbody part0_inst = Instantiate(Part0, transform.position, Quaternion.identity) as Rigidbody;
-                translation += new Vector3(4, 0);
-                transform.Translate(translation);
+                if (LastObject != 5)
+                {
+                    Rigidbody part0_inst = Instantiate(Part0, transform.position, Quaternion.identity) as Rigidbody;
+                    translation += new Vector3(3, 0);
+                    transform.Translate(translation);
+                    LastObject = 0;
+                }
             }
 
             if (place == 1)
@@ -63,20 +85,23 @@ public class hackObject : MonoBehaviour
                 Rigidbody part1_inst = Instantiate(Part1, transform.position, Quaternion.identity) as Rigidbody;
                 translation += new Vector3(4, 0);
                 transform.Translate(translation);
+                LastObject = 1;
             }
 
             if (place == 2)
             {
                 Rigidbody part2_inst = Instantiate(Part2, transform.position, Quaternion.identity) as Rigidbody;
-                translation += new Vector3(0, 3);
+                translation += new Vector3(0, 2);
                 transform.Translate(translation);
+                LastObject = 2;
             }
 
             if (place == 3)
             {
                 Rigidbody part3_inst = Instantiate(Part3, transform.position, Quaternion.identity) as Rigidbody;
-                translation += new Vector3(0, -3);
+                translation += new Vector3(0, -2);
                 transform.Translate(translation);
+                LastObject = 3;
             }
 
             if (place == 4)
@@ -84,13 +109,15 @@ public class hackObject : MonoBehaviour
                 Rigidbody part4_inst = Instantiate(Part4, transform.position, Quaternion.identity) as Rigidbody;
                 translation += new Vector3(4, 0);
                 transform.Translate(translation);
+                LastObject = 4;
             }
 
             if (place == 5)
             {
                 Rigidbody part5_inst = Instantiate(Part5, transform.position, Quaternion.identity) as Rigidbody;
-                translation += new Vector3(0, 3);
+                translation += new Vector3(0, 2);
                 transform.Translate(translation);
+                LastObject = 5;
             }
         }
     }
